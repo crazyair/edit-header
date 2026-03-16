449ae995f70fb4fbcdb23f858f9529f0fe0830fa
黄建峰 <645381995@qq.com>
2026-03-16 15:49:30 +0800

    feat: 临时修复 values 丢失字段问题

--- components/RulesForm.tsx
+++ components/RulesForm.tsx
@@ -100,7 +100,16 @@ const RulesForm = ({ settings, ruleData, onChange }: { settings: settingsType; r
   }, []);
 
   return (
-    <Form form={form} initialValues={ruleData} onValuesChange={(_, values) => onChange(values)}>
+    <Form
+      form={form}
+      initialValues={ruleData}
+      // onValuesChange={(_, values) => onChange(values)}
+      onValuesChange={() => {
+        setTimeout(() => {
+          onChange(form.getFieldsValue());
+        });
+      }}
+    >
       <Form.List name="rules">
         {(fields, { add, remove }) => (
           <Flex vertical gap={8}>
